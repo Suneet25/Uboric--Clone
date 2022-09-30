90fe8548ec7822152da4e8c3db14d767f3a7cbf5
Suneet25 <suneetpanigrahi2505@gmail.com>
2022-09-30 23:35:39 +0530

    added day-4 task

--- myapp/src/components/middle.jsx
+++ myapp/src/components/middle.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React,{useState} from 'react';
+import {FaAngleLeft,FaAngleRight} from "react-icons/fa"
 const  container=
 {
     height:"300px",
@@ -14,6 +15,19 @@ border:"2px solid black"
 
 
 export const Middle = () => {
+
+  const [isHovering, setIsHovering] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovering(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovering(false);
+  };
+
+
+
   return (
     <div>
   <div className='container' style={container}>
@@ -25,7 +39,42 @@ export const Middle = () => {
         <h1 >Shop By Category</h1>
         <p><u style={{color:"#ff6f61"}}><b>Clothing</b></u></p>
     </div>
-
+    <div className='categories' style={{display:"flex",justifyContent:"space-evenly",alignItems:"center",marginTop:"80px"}}>
+      <div><h3         style={{
+            color: isHovering ? '#ff6f61' : 'black'
+            
+          }}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave} >Men's Fashion (120)</h3></div>
+      <div><h3     style={{
+            color: isHovering ? '#ff6f61' : 'black'
+            
+          }}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}   >shirt & boxer (4)</h3></div>
+      <div><h3     style={{
+            color: isHovering ? '#ff6f61' : 'black'
+            
+          }}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}  >T-shirts (13)</h3></div>
+      <div><h3     style={{
+            color: isHovering ? '#ff6f61' : 'black'
+            
+          }}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}  >Trunks & Briefs (59)</h3></div>
+      <div><h3     style={{
+            color: isHovering ? '#ff6f61' : 'black'
+            
+          }}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}  >Shirts (41)</h3></div>
+    </div>
+    <div className='arrows' style={{display:"flex",justifyContent:"space-between",marginTop:"30px"}}>
+      <div style={{marginLeft:"100px"}} ><FaAngleLeft style={{ width:"42px",height:"42px",color:"#a0a0a0"}}  /></div>
+      <div style={{marginRight:"100px"}}><FaAngleRight style={{ width:"42px",height:"42px" ,color:"#a0a0a0"}} /></div>
+    </div>
     </div>
   
   )
